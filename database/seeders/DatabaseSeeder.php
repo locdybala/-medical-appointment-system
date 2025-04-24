921bd618374d4522d1332fd7c3d3d01fba631bc2
LocDybala11 <locdybala11.it@gmail.com>
2025-04-25 00:20:51 +0700

    Add

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,6 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
-            RoleSeeder::class,
             AdminSeeder::class,
             SpecialtySeeder::class,
             RoomSeeder::class,
