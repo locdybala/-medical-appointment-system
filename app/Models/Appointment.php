2537fd2ab31271a7ee6533209d777188c566e2d1
locnd <locnd@softdreams.vn>
2025-05-28 16:18:24 +0700

    Add

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Appointment extends Model
 {
@@ -17,6 +17,7 @@ class Appointment extends Model
         'fee',
         'is_paid',
         'notes',
+        'status'
     ];
 
     protected $casts = [
